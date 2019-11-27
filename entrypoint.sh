521d759cd1abebaf6e138808b3aa1dbcd13fd847
danthe1st <daniel@wwwmaster.at>
2019-11-27 18:08:11 +0100

    debug output

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ prefix="/"
 text=`jq -r ".comment.body" "$GITHUB_EVENT_PATH"`
 
 echo "does $text start with $prefix ?"
+echo "if [[ $text == ${prefix}* ]]; then"
 if [[ $text == ${prefix}* ]]; then
   # is command
   echo "this is a command"
