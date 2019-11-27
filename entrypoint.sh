b6d5bd192e5e523009ce5142493af9ae4b464613
danthe1st <daniel@wwwmaster.at>
2019-11-27 17:57:01 +0100

    jq

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,14 @@
-#!/bin/sh -l
+#!/bin/sh
 
-#echo "get $1"
-#curl -L "$1" > run.jar
-#java -jar run.jar
-
-echo 'setting PATH'
 PATH="$PATH:/lib"
-echo "set PATH to $PATH"
 
 SKIP=0
 PREFIX="/"
-echo "if"
-if [[ $str == ${PREFIX}* ]]; then
+
+text=`jq -r ".comment.body" "$GITHUB_EVENT_PATH"`
+
+echo "$text"
+if [[ $text == ${PREFIX}* ]]; then
   # is command
   echo "this is a command"
   sendAPI
