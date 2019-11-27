4627fd9278c8497278dd4be4ca6b6fedfb9de2f2
danthe1st <daniel@wwwmaster.at>
2019-11-27 17:17:11 +0100

    send api

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,19 @@
 #!/bin/sh -l
 
-echo "get $1"
-curl -L "$1" > run.jar
-java -jar run.jar
+#echo "get $1"
+#curl -L "$1" > run.jar
+#java -jar run.jar
+
+export PATH=$PATH:/lib
+
+SKIP=0
+PREFIX="$1"
+
+if [[ $str == "${PREFIX}*" ]]; then
+  # is command
+  echo "this is a command"
+  sendAPI
+else
+  echo "no command - skipping"
+  exit $SKIP
+fi
