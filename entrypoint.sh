b4c21d04fce2abd97609b6bfeee1178ef64e8bfe
danthe1st <daniel@wwwmaster.at>
2019-11-28 16:31:48 +0100

    first command :)

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,14 +11,24 @@ fi
 
 SKIP=0
 prefix="/"
+prefixLen=1
 
 text=`jq -r ".comment.body" "$GITHUB_EVENT_PATH"`
 
 if [[ $text == ${prefix}* ]]; then
   # is command
-  echo "this is a command"
-  sendAPI
+  echo "$text is a command"
+  text="${text:$prefixLen}"
+  cmdName="`cut -d' ' -f1`"
+  cmdName="`echo $cmdName|sed 's/[^a-zA-Z0-9]//g'`"
+  args="`cut -d' ' -f2-`"
+  echo "executing command $cmdName with arguents $args"
+  if [-x "/commands/$cmdName" ]; then
+  	bash -c "/commands/$cmdName $args" && echo "executed command successfully" || echo "command errored with exit code $?"
+  else
+	echo "command not existant or executable" >/dev/stderr
+	exit -1
 else
-  echo "no command - skipping"
+  echo "$text is no command - skipping"
   exit $SKIP
 fi
