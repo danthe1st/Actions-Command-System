5801c7d0f98f97c3b3c99e961a410a42256c45b5
danthe1st <daniel@wwwmaster.at>
2019-11-28 16:50:26 +0100

    some debug output

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,13 +18,14 @@ text=`jq -r ".comment.body" "$GITHUB_EVENT_PATH"`
 if [[ $text == ${prefix}* ]]; then
   # is command
   echo "$text is a command"
-  text="${text:$prefixLen}"
-  cmdName="`echo $text|cut -d' ' -f1`"
-  cmdName="`echo $cmdName|sed 's/[^a-zA-Z0-9]//g'`"
-  args="`echo $text|cut -d' ' -f2-`"
+  text="${text:$prefixLen}"|| ( echo "err stripping prefix" &&exit $?)
+  cmdName="`echo $text|cut -d' ' -f1`"|| ( echo "err getting name" &&exit $?)
+  cmdName="`echo $cmdName|sed 's/[^a-zA-Z0-9]//g'`"|| ( echo "err stripping name" &&exit $?)
+  args="`echo $text|cut -d' ' -f2-`"|| ( echo "err getting args" &&exit $?)
   echo "executing command $cmdName with arguents $args"
   if [ -x "/commands/$cmdName" ]; then
-  	bash -c "/commands/$cmdName $args" && echo "executed command successfully" || echo "command errored with exit code $?"
+  	ls -l /commands
+	bash -c "/commands/$cmdName $args" && echo "executed command successfully" || echo "command errored with exit code $?"
   else
 	echo "command not existant or executable" >/dev/stderr
 	exit -1
