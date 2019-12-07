8e20d41cfe5a0be12038a20c27b6e538a08ae323
danthe1st <daniel@wwwmaster.at>
2019-12-07 11:51:49 +0100

    docs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# This script is called whenever the action is executed
+# It tests if an issue comment is a command and executes a bash script at /commands/<name> if it is a command
+# exported Variables:
+#   GITHUB_TOKEN: the GitHub token(see $1)
+#   REPO_FULLNAME: the name of the Repository: <username>/<repo-name>
+#   MESSAGE_AUTHOR: the username of the author of the comment
+#   GH_EVENT_PATH: the json path in the GITHUB_EVENT_PATH where the data of the event can be found
+#   API_PATH: the path where requests to the api should be sent to
+# Arguments:
+#   - The GitHub token to be used
 set -e
 
 PATH="$PATH:/lib"
@@ -47,7 +57,7 @@ if [[ $text == ${prefix}* ]]; then
 	( bash -c "/commands/$cmdName $args" && echo "executed command successfully" >/dev/stderr ) || ( code=$?; echo "command errored with exit code $code" >/dev/stderr && exit $code )
   else
 	echo "command not existant or executable" >/dev/stderr
-	exit 1
+	exit $SKIP
   fi
 else
   echo "$text is no command - skipping">/dev/stderr
